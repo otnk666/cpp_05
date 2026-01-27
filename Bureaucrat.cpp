18cbc804142d9289ff117f6eba5d21679d78a67a
otnk666 <128681288+otnk666@users.noreply.github.com>
2026-01-27 17:03:11 +0900

    first commit

---
+++ Bureaucrat.cpp
@@ -0,0 +1,17 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat()
+{}
+
+Bureaucrat::Bureaucrat(const std::string &name, const int &grade)
+{
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other)
+{}
+
+Bureaucrat::Bureaucrat &operator=(const Bureaucrat& other)
+{}
+
+Bureaucrat::~Bureaucrat()
+{}
